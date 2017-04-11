b9645d87ee2d8abf5e0f1b3a772d3964fb59bc23
Freyr <giuliano150@hotmail.com>
2017-04-11 13:48:08 -0300

    pymongo tests

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import telebot, requests, wolframalpha, persistence, sys, os, conversationTools
+import telebot, requests, wolframalpha, persistence, sys, os, conversationTools, pymongo
 from telebot import types
 from watson_developer_cloud import ConversationV1, LanguageTranslatorV2
 import time
@@ -7,16 +7,15 @@ import time
 #b = Bridge('172.27.0.116')
 #b.connect()
 
-databaseFile = 'pyping-v1.db'
-databasePath = './' + databaseFile
 
-database = persistence.Database(databasePath)
+client = pymongo.MongoClient('mongodb://admin:XJROENXBKWZXDWMC@bluemix-sandbox-dal-9-portal.7.dblayer.com:25382')
+db = client.user_dict
 
-def getLights():
-    lights = b.get_light_objects('name')
-    for light in lights:
-        yield light
-    pass
+# def getLights():
+#     lights = b.get_light_objects('name')
+#     for light in lights:
+#         yield light
+#     pass
 
 privilegedChats = [42789923, 25863480, 332992877]
 def hasAccess(cid):
